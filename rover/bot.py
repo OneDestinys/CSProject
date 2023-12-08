3f09db3344c20db71ea4e02df82264ac021c1f78
Jacob <122861394+OneDestinys@users.noreply.github.com>
2023-12-07 23:56:59 -0500

    Update bot.py

--- rover/bot.py
+++ rover/bot.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import discord
 from translate import Translator
+import requests
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,10 +19,16 @@ async def ping(ctx):
 
 @client.command()
 async def translate(ctx, *, text):
-    translator = Translator(from_lang="fr",to_lang="en-us")
+    translator = Translator(from_lang="de",to_lang="en-us")
     try:
         translated_text = translator.translate(text)
         await ctx.send(translated_text)
     except Exception as e:
         await ctx.send("Sorry, I can't translate that right now.")
+@client.command() 
+async def dog(ctx):
+    response = requests.get('https://dog.ceo/api/breeds/image/random')
+    data = response.json()
+    dog_url = data['message']
+    await ctx.send(dog_url)
 client.run(TOKEN)
